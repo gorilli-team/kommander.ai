073e4659244f3b51eeac34fd3982cc0b2f98e98f
SamuelePassiatore <samuw.978@gmail.com>
2025-03-17 17:44:52 +0100

    add header component

---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+      <header className="h-16 px-6 flex items-center justify-end bg-gray-800 border-b border-gray-600">  
+        <div className="flex items-center">
+          Header
+        </div>
+      </header>
+    );
+  }
