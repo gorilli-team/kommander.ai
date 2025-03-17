16c9da652de96da9a3e291b9a97b36eacba8fba4
SamuelePassiatore <samuw.978@gmail.com>
2025-03-17 17:45:24 +0100

    add pages

---
+++ frontend/src/app/pages/Docs.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Docs() {
+  return (
+    <div>
+      <h2>Docs</h2>
+    </div>
+  );
+}
---
+++ frontend/src/app/pages/Widget.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Widget() {
+  return (
+    <div>
+      <h2>Widget</h2>
+    </div>
+  );
+}
