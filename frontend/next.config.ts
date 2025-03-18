30f23199bbda60da03b5ac34261cb9d2551d3f5c
SamuelePassiatore <samuw.978@gmail.com>
2025-03-18 15:08:17 +0100

    update next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: {
-    appIsrStatus: false
+    appIsrStatus: false,
+    buildActivity: false,
   }
 };
 
